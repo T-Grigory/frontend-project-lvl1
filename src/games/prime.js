6942ef25cf181fd444447877479d0121497e2a7f
Grigory <tydykov2022@mail.ru>
2022-08-17 15:34:11 +0700

    fix

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,32 +2,31 @@ import { NUMBER_OF_ROUNDS, engine } from '../index.js';
 import { getRandomInt } from '../random.js';
 
 function isPrime(number) {
-    for (let i = 2; i < number; i+=1) {
-        if (number % i === 0) {
-            return false;
-        }
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
-
 export default function primeGameStart() {
-    const data = [];
+  const data = [];
 
-    for (let i = 0; i < NUMBER_OF_ROUNDS; i+=1) {
-        const currentData = [];
+  for (let i = 0; i < NUMBER_OF_ROUNDS; i += 1) {
+    const currentData = [];
 
-        const number = getRandomInt();
+    const number = getRandomInt();
 
-        const result = isPrime(number) ? 'yes' : 'no';
+    const result = isPrime(number) ? 'yes' : 'no';
 
-        currentData.push(number);
-        currentData.push(result);
+    currentData.push(number);
+    currentData.push(result);
 
-        data.push(currentData);
-    }
+    data.push(currentData);
+  }
 
-    const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-    engine(data, rule);
+  engine(data, rule);
 }
--- src/games/progression.js
+++ src/games/progression.js
@@ -1,38 +1,37 @@
 import { NUMBER_OF_ROUNDS, engine } from '../index.js';
 import { getRandomInt } from '../random.js';
+
 const PROGRESSION_LENGTH = 10;
 
 function getProgression(start, step) {
+  const progression = [];
+  let number = start;
+  for (let i = 0; i < PROGRESSION_LENGTH; i += 1) {
+    number += step;
+    progression.push(number);
+  }
 
-    const progression = [];
-    let number = start;
-    for (let i = 0; i < PROGRESSION_LENGTH; i += 1) {
-        number += step;
-        progression.push(number);
-    }
-
-    return progression;
+  return progression;
 }
 
 export default function progressionGameStart() {
-    const data = [];
+  const data = [];
 
-    for (let i = 0; i < NUMBER_OF_ROUNDS; i+=1) {
-        const currentData = [];
-        const startProgression = getRandomInt();
-        const stepProgression = getRandomInt(10);
-        const missingNumberIndex = getRandomInt(10);
+  for (let i = 0; i < NUMBER_OF_ROUNDS; i += 1) {
+    const currentData = [];
+    const startProgression = getRandomInt();
+    const stepProgression = getRandomInt(10);
+    const missingNumberIndex = getRandomInt(10);
 
-        const progression = getProgression(startProgression, stepProgression);
-        const result = String(progression[missingNumberIndex]);
-        progression[missingNumberIndex] = '..';
+    const progression = getProgression(startProgression, stepProgression);
+    const result = String(progression[missingNumberIndex]);
+    progression[missingNumberIndex] = '..';
 
-        currentData.push(progression.join(' '));
-        currentData.push(result);
+    currentData.push(progression.join(' '));
+    currentData.push(result);
 
-        data.push(currentData);
-
-    }
-    const rule = 'What number is missing in the progression?';
-    engine(data, rule);
+    data.push(currentData);
+  }
+  const rule = 'What number is missing in the progression?';
+  engine(data, rule);
 }
